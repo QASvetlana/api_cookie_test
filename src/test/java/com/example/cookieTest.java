d60b540042ceddd0bd56ff7370e593c6b6684a20
Sveta Andreeva <89111378147v@yandex.com>
2022-11-09 00:06:05 +0300

    add test

---
+++ src/test/java/com/example/cookieTest.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.Headers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class cookieTest {
+    @Test
+    public void testCookie(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+             //   .post("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+        int userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+
+        assertEquals(200, responseGetAuth.statusCode(), "Unexpected status code");
+        assertTrue(cookies.containsKey("auth_sid"), "Response doesn't have 'auth_sid' cookies");
+        assertTrue(headers.hasHeaderWithName("x-csrf-token"), "Response doesn't have 'x-csrf-token' header");
+        assertTrue(responseGetAuth.jsonPath().getInt("user_id") > 0, "User id should be greater then 0");
+
+        JsonPath responseCheckAuth = RestAssured
+                .given()
+                .header("x-csrf-token", responseGetAuth.getCookie("auth_sid"))
+                .get("https://playground.learnqa.ru/api/user/auth")
+              // .get("https://playground.learnqa.ru/api/homework_cookie")
+                .jsonPath();
+
+        int userIdOnCheck = responseCheckAuth.getInt("user_id");
+        assertTrue(userIdOnCheck > 0, "Unexpected user id" + userIdOnCheck);
+
+        assertEquals(
+                userIdOnAuth,
+                userIdOnCheck,
+                "user id from auth request is not equal to user_id from check request"
+        );
+    }
+}
